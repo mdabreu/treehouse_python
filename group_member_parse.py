308bdf20f67d3aee4320882fd6d0a0a66a7a0c11
mdabreu <35778755+mdabreu@users.noreply.github.com>
2021-05-05 18:25:52 -0400

    Group Member Parse
    Treehouse problem

---
+++ group_member_parse.py
@@ -0,0 +1,24 @@
+## code to parse group names from bands
+
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
+]
+
+
+# Your code here
+
+g = 0
+
+
+for member in musical_groups:
+    parse_group = ", ".join(musical_groups[g])
+    print(parse_group)
+    g = g +1
+                        
+    
